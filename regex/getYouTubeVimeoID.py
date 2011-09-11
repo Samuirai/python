271279a4ac213c633ecb834306447af558ba8873
Fabian Faessler <fabi@fabif.de>
2011-09-12 00:31:22 +0200

    added comments and cleaned the code

--- regex/getYouTubeVimeoID.py
+++ regex/getYouTubeVimeoID.py
@@ -1,5 +1,13 @@
+# author: Samuirai (Fabian Faessler)
+# email: fabi@fabif.de
+""" 
+This is a RegularExpression test, which extracts a Video ID from YouTube or Vimeo URLs. 
+It is for my website www.penspinningonline.com, where Users can add their videos, and they do it with all kind of Video URLs.
+"""
+
 import re
 
+# Some YouTube Urls
 y = [
      "http://www.youtube.com/watch?v=M14Pnulj-j8&feature=feedf",
      "www.youtube.com/watch?v=M14Pnulj-j8&feature=feedf",
@@ -14,6 +22,7 @@ y = [
      "youtube.com/v/M14Pnulj-j8",
     ]
 
+#Some vimeo.com URLs
 v = [
      "http://vimeo.com/28257653",
      "http://www.vimeo.com/28257653",
@@ -22,11 +31,9 @@ v = [
      "28257653"
     ]
 
-
+#regex for youtube and for vimeo
 regex_y = r'.*(?:v=|/v/|^)(?P<id>[^&]*)'
 regex_v = r'.*/(?P<id>\d+)'
-#regex = r"^((?:.*)(?=&)|(?:.*)$)"
-#regex = r".*v=(.*)&|.*v=(.*)[^&]?"
 
 #print "RegularExpression: "+regex+"\n_____________"
 
@@ -36,18 +43,14 @@ for url in y:
     erg = regex_y.match(url)
     #print "## "+url+" ##"
     try:
-        #print "( ) "+erg.group()
         if erg.group('id'): print erg.group('id')+" | "+url
     except: pass
-    #assert erg == "M14Pnulj-j8"
 
 regex_v = re.compile(regex_v)
 
 for url in v:
     erg = regex_v.match(url)
-    #print "## "+url+" ##"
     try:
         #print "( ) "+erg.group()
         if erg.group('id'): print erg.group('id')+" | "+url
     except: pass
-    #assert erg == "M14Pnulj-j8"
