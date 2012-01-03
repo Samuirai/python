6f7409a4fcaa9c9fa0ef4dab1d212352be5a736a
Fabian Faessler <fabi@fabif.de>
2012-01-03 04:41:32 +0100

    added some information extraction

--- vvs/vvs.py
+++ vvs/vvs.py
@@ -14,3 +14,16 @@ bytes = create_7bits(bits)
 print "[Splitted]\n  "+str(bytes)+"\n"
 iso,left = create_7bit2a(bytes)
 print "[ASCII String]\n  "+iso+"\n  bits left: "+left
+info = iso[1:].split('^')
+
+print "\n[Verbundspass Info]"
+print "  Prefix           "+info[0]
+print "   ?               "+info[1][0:3]
+print "  Preisstufe       "+info[1][3:5]
+print "  Pass Nr          "+info[1][5:12]+"-"+info[1][12]
+print "   ?               "+info[2][0:6]
+print "  Gueltig bis      "+info[2][8:10]+"/"+info[2][6:8]
+print "   ?               "+info[3]
+print "  Verkaufsstelle   "+info[4]
+print "  Ausstelldatum    "+info[5][4:6]+"."+info[5][2:4]+"."+info[5][0:2]
+print "  Geltungsbereiche "+info[5][6:8]+" "+info[5][8:10]+" "+info[5][10:12]+" "+info[5][12:14]+" "+info[5][14:16]+" "+info[5][16:18]+" "+info[5][18:20]+" "+info[5][20:22]+" "+info[5][22:24]+" "+info[5][24:26]
