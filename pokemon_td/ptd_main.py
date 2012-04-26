65089d9ff6311de9a1473ccc848f469c23e7bc37
Fabian Faessler <fabi@fabif.de>
2012-04-26 10:04:04 +0200

    pokemon td hack update

--- pokemon_td/ptd_main.py
+++ pokemon_td/ptd_main.py
@@ -1,10 +1,9 @@
 import ptd_info
 from ptd import smrrd_PTD
 
+ptd = smrrd_PTD(email='EMAIL',password='PASSWORD',version='650',aes_key='lkafd8halkf',verbose_lvl=1)
 
-ptd = smrrd_PTD(email='pokemon@smrrd.de',password='password',version='650',aes_key='lkafd8halkf',verbose_lvl=1)
-ptd.register(email='gahsdkf@dgjas.de',password='fgajs')
-ptd.info()
+ptd.login()
 
 ptd.catch(num=1,lvl=100, shiny=True)
 ptd.catch(num=2,lvl=100, m1=123, m2=42)
